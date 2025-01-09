fbef909ea9922bb7b45a64e38ec5faafbfa9692f
Jason <jko1@hotmail.com>
2025-01-09 14:05:01 -0600

    first commit

---
+++ src/main/java/JavaMavReports/ReportDemo/App.java
@@ -0,0 +1,13 @@
+package JavaMavReports.ReportDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/JavaMavReports/ReportDemo/ExtendReports.java
@@ -0,0 +1,44 @@
+package JavaMavReports.ReportDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class ExtendReports {
+	ExtentReports spark;
+	
+	@BeforeTest
+	public void config() throws Throwable
+    {
+		String path = System.getProperty("user.dir") + "//reports//index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Jason Report Results");
+		reporter.config().setDocumentTitle("Jason Title Results");
+		
+		spark = new ExtentReports();
+		spark.attachReporter(reporter);
+		spark.setSystemInfo("Tester","Jason Alfaro");
+		
+	}
+	
+	@Test
+	public void initialDemo()
+	{
+		ExtentTest test = spark.createTest("initialDemo");
+		WebDriver driver = new FirefoxDriver();			
+		//driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		///test.addScreenCaptureFromBase64String(null);
+		test.fail("Result do not match");
+		spark.flush();
+	}
+
+}
