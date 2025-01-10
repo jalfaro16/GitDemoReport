6cfb8dd5c7e2b5438aff171b320ea00a55d1bf7e
Jason <jko1@hotmail.com>
2025-01-09 23:37:59 -0600

    final commit from spceX

---
+++ src/test/java/JavaMavReports/ReportDemo/DemoxGitnewclass.java
@@ -0,0 +1,15 @@
+package JavaMavReports.ReportDemo;
+
+import org.testng.annotations.Test;
+
+public class DemoxGitnewclass {
+
+	@Test
+	public void iamtheNew()
+	{
+	//Added fropm spaceX
+			System.out.println("Change11");
+			System.out.println("Change12");
+}
+
+}
